acab6d983b776d76bb37c92fcd82f951705c6844
yoshi-corleone <yoshi.corleone@gmail.com>
2018-06-19 23:19:26 +0900

    albumの追加準備

--- editor/models.py
+++ editor/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 from recorder.models import Point
 
@@ -6,6 +7,7 @@ from recorder.models import Point
 class Course(models.Model):
     name = models.CharField(max_length=255)
     points = models.ManyToManyField(Point)
+    # user = models.ForeignKey(User)
 
     def __str__(self):
         return self.name
